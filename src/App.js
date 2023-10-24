8a27814176db6e6d5e13cee69f934964da42bab4
hazelglaine <hazelglainesales@gmail.com>
2023-10-24 09:56:22 +0800

    Added Github link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Weather Application</h1>
+      <footer>
+        This project was coded by Hazel and is{" "}
+        <a
+          href="https://github.com/hazelglaine/project-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>{" "}
+        in Github.
+      </footer>
+    </div>
+  );
 }
 
 export default App;
