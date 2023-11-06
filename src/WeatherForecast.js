4bdf975151d308db0c179621f01b9846437782e6
hazelglaine <hazelglainesales@gmail.com>
2023-11-06 23:02:23 +0800

    Updated WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -26,6 +26,8 @@ export default function WeatherForecast(props) {
                 <WeatherForecastDay forecastData={dailyForecast} />
               </div>
             );
+          } else {
+            return null;
           }
         })}
       </div>
